d67ee6b471af79c7e369f29a51eadb6a5fcedeba
Bogdan Grozoiu <grozoiu@hotmail.com>
2022-11-22 16:18:02 +0100

    path correction

--- DevOps/helpers/Invoke-Pester.ps1
+++ DevOps/helpers/Invoke-Pester.ps1
@@ -14,7 +14,7 @@ Write-Verbose -Message ('Testing of "{0}" API has started' -f $APIUrl) -Verbose
 
 # Get-ChildItem env: | fl
 
-$testScriptsPath = Join-Path -Path $TestResultsPath -ChildPath 'Tests'
+$testScriptsPath = Join-Path -Path $TestResultsPath -ChildPath 'DevOps/Tests'
 $testResultsFile = Join-Path -Path $TestResultsPath -ChildPath 'TestResults.Pester.xml'
 
 $PesterVersion = '5.3.3'
